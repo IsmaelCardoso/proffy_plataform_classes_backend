d5cfe2f817cb68a6a76f16f1c1dc2a544229ba77
Ismael Cardoso <ismael.cardosos@gmail.com>
2020-08-07 18:33:28 -0300

    Creation:  - Project structure - start;   - Creating DB  - Creating tables and relocating the DB: users,classes, class_schedule and connection  - Routes/CRUD  - Server

---
+++ src/utils/convertHourToMinutes.ts
@@ -0,0 +1,9 @@
+const convertHouerToMinutes = (time: string) => {
+  const [hour, minutes] = time.split(":").map(Number);
+
+  const timeInMinutes = hour * 60 + minutes;
+
+  return timeInMinutes;
+};
+
+export default convertHouerToMinutes;
